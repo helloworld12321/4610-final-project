e0a6e4e53c9fbd17cc81978201e8aa78b1760c90
Joe Walbran <walbr037@morris.umn.edu>
2021-04-18 17:12:28 -0500

    Make sure to export baseUrl
    Oops, I wasn't exporting this before :( All of the files were seeing
    `baseUrl` as an undefined variable. That should be fixed now!

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const baseUrl = 'https://08noe16b16.execute-api.us-east-1.amazonaws.com/Prod';
+export const baseUrl = 'https://08noe16b16.execute-api.us-east-1.amazonaws.com/Prod';
 console.log(`The base URL is ${baseUrl}.`);
