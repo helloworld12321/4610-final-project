4047f9df3f4643aaa39c66c479caa8e620f1c752
Joe Walbran <walbr037@morris.umn.edu>
2021-04-17 19:36:14 -0500

    Adapt Prof. Nic and Prof. Peter's starting code
    The professors gave us some HTML and JavaScript to use for the frontend.
    I've included that code here, moderately edited.
    
    (Mostly, I changed it so that the JavaScript files are bundled using
    webpack instead of being loaded from individual <script> tags.)

---
+++ frontend/src/index.js
@@ -0,0 +1,103 @@
+import $ from 'jquery';
+import './index.css';
+
+const baseUrl = 'https://08noe16b16.execute-api.us-east-1.amazonaws.com/Prod/';
+console.log(`The base URL is ${baseUrl}.`);
+
+// Set up the functions to be called when the user clicks on any
+// of the three buttons in our (very simple) user interface.
+$('#generate-random-routes').on('click', makeRandomRoutes);
+$('#get-best-routes').on('click', getBestRoutes);
+$('#get-route-by-id').on('click', getRouteById);
+
+/**
+ * This generates a single random route by POSTing the runId and generation to
+ * the `/routes` endpoint. It's asynchronous (like requests across the network
+ * typically are), and the showRoute() function is called when the request
+ * response comes in.
+ */
+function makeRandomRoute(runId, generation) {
+    $.ajax({
+        method: 'POST',
+        url: `${baseUrl}/routes`,
+        data: JSON.stringify({ runId, generation}),
+        contentType: 'application/json',
+        // When a request completes, call `showRoute()` to display the
+        // route on the web page.
+        success: showNewRoute,
+        error: showErrorMakingRoute,
+    });
+}
+
+/**
+ * Generates a collection of new routes, where the number to generate (and the
+ * runId and generation) are specified in the HTML text fields. Note that we
+ * don't do any kind of sanity checking here, when it would make sense to at
+ * least ensure that `numToGenerate` is a non-negative number.
+ */
+function makeRandomRoutes() {
+    const runId = $('#runId-text-field').val();
+    const generation = $('#generation-text-field').val();
+    const numToGenerate =$('#num-to-generate').val();
+    // Reset the contents of `#new-route-list` so that it's ready for
+    // `showRoute()` to "fill" it with the incoming new routes.
+    $('#new-route-list').text('');
+    for (let i = 0; i < numToGenerate; i++) {
+        makeRandomRoute(runId, generation);
+    }
+}
+
+/**
+ * When a request for a new route completes successfully, add a `<li>…</li>`
+ * element to `#new-route-list` with that routes information.
+ */
+function showNewRoute(result) {
+    console.log(`New route received from API: ${result}`);
+    const routeId = result.routeId;
+    const length = result.length;
+    $('<li></li>')
+        .text(`Generated route ${routeId} with length ${length}`)
+        .appendTo('#new-route-list');
+}
+
+/**
+ * When a request for a new route completes unsuccessfully, add an element to
+ * the `#new-route-list` with an error message.
+ */
+function showErrorMakingRoute(xhr, _, errorThrown) {
+    console.error(`Error generating random route: ${errorThrown}`);
+    console.error(`Response: ${xhr.responseText}`);
+    $('<li></li>').text(`Error: ${errorThrown}`).appendTo('#new-route-list');
+}
+
+/**
+ * Make a `GET` request that gets the K best routes.
+ *
+ * The form of the `GET` request is:
+ *   …/best?runId=…&generation=…&numToReturn=…
+ *
+ * This request will return an array of
+ *   { length: …, routeId: … }
+ *
+ * You should add each of these to `#best-route-list` (after clearing it
+ * first).
+ */
+function getBestRoutes() {
+    alert('You need to implement getBestRoutes()');
+}
+
+/**
+ * Make a `GET` request that gets all the route information for the given
+ * `routeId`.
+ *
+ * The form of the `GET` request is:
+ *   …/routes/:routeId
+ *
+ * This request will return a complete route JSON object.
+ *
+ * You should display the returned information in `#route-by-id-elements`
+ * (after clearing it first).
+ */
+function getRouteById() {
+    alert('You need to implement getRouteById()');
+}
