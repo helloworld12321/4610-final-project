2c020306f14a7057308f8a7fdd96355f43c118ec
Joe Walbran <walbr037@morris.umn.edu>
2021-04-22 09:52:57 -0500

    Change AWS URL
    Since we needed to tear down the old stack.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'https://08noe16b16.execute-api.us-east-1.amazonaws.com/Prod';
+export const baseUrl = 'https://rwtnoc36j9.execute-api.us-east-1.amazonaws.com/Prod';
 console.log(`The base URL is ${baseUrl}.`);
