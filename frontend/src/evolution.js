40ee596a03fee10e598c7edecca1fd9efafd6252
Joe Walbran <walbr037@morris.umn.edu>
2021-05-04 23:48:13 -0500

    Disable the "run evolution" button during a run
    So that the user doesn't accidentally run two evolutions at the same
    time--that would be weird.
    
    (Note that we haven't actually implemented "runEvolution" yet.)

---
+++ frontend/src/evolution.js
@@ -0,0 +1,5 @@
+
+export async function runEvolution() {
+    // TODO
+    return new Promise(resolve => setTimeout(resolve, 1000));
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,9 @@
  */
 
 import $ from 'jquery';
-import './index.css';
+import { runEvolution } from './evolution';
 
+import './index.css';
 import * as content from './page/content';
 import * as form from './page/form-fields';
 import * as map from './page/map';
@@ -12,36 +13,29 @@ import * as map from './page/map';
 map.initMap();
 
 $('#run-evolution').on('click', () => {
-    // TODO
-    // For the moment, we'll just test that everything works.
-
-    const numGenerations  = form.getNumGenerations();
-    const numParents = form.getNumParents();
-    const populationSize = form.getPopulationSize();
-
-    content.runId.clear();
-    content.runId.set('abacabadabacaba')
-
-    content.bestRouteId.clear();
-    content.bestRouteId.set('foo');
-
-    content.bestLength.clear();
-    content.bestLength.set('bar');
-
-    content.bestPath.clear();
-    content.bestPath.set('baz');
-
-    content.bestRouteCities.clear();
-    content.bestRouteCities.add('quux');
-
-    content.currentThreshold.clear();
-    content.currentThreshold.set('wibbly-wobbly');
-
-    content.currentGeneration.clear();
-    content.currentGeneration.set('timey-wimey');
-
-    content.newRoutesList.clear();
-    content.newRoutesList.add(`numGenerations: ${typeof numGenerations} ${numGenerations}`);
-    content.newRoutesList.add(`numParents: ${typeof numParents} ${numParents}`);
-    content.newRoutesList.add(`populationSize: ${typeof populationSize} ${populationSize}`);
+    // Disable the button to prevent the user from running several evolutions
+    // at once. (See async notes below.)
+    $('#run-evolution').prop('disabled', true);
+    runEvolution().finally(() => {
+        $('#run-evolution').prop('disabled', false);
+    });
 });
+
+// Async notes:
+//
+// From some preliminary testing, it looks like, when the user clicks a
+// button, they aren't able to click it again until after that button's
+// handler's task on the event loop has completed. (For our handler, that
+// happens right after we register the "finally" callback with the
+// `runEvolution` promise.)
+//
+// So, if the user clicks the "Run evolution" button, they can't click it
+// again until the current evolution is completely done.
+//
+// They can't click the button again *before* the click handler has finished.
+// (The browser prevents that.)
+//
+// And they can't click the button again *after* the click handler has
+// finished. (Because at that point the button will be disabled.)
+//
+// (I tested this behavior on a Mac in Chrome, Safari, and Firefox.)
